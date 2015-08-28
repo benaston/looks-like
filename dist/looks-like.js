3d24b9f1203e23be32a57ea90e3d058ea569fc75
Ben Aston <ben@bj.ma>
2015-08-28 19:49:13 +0100

    Improved support for arrays and functions. Improved failure mode. API change.

--- dist/looks-like.js
+++ dist/looks-like.js
@@ -9,45 +9,168 @@
 
     'use strict';
 
-    function looksLike(o, example) {
-        if (!isObject(o)) {
+    /**
+     * Ensures `o` is defined and "looks like" 
+     * `example`. If validation fails, throws 
+     * an exception containing the name of the 
+     * key that caused the validation failure.
+     */
+    function looksLike(o, example, options) {
+        var returnValue = {};
+
+        if (!isObject(o, options)) {
             throw 'o must be an object.';
         }
-        if (!isObject(example)) {
+        if (!isObject(example, options)) {
             throw 'example must be an object.';
         }
 
-        return areAlike(o, example);
+        returnValue.value = areAlike(o, example, options, returnValue);
+
+        return returnValue;
     }
 
-    function areAlike(o, example) {
-        return Object.keys(example)
-            .every(isAlike.bind(null, o, example));
-    }
-
-    function isAlike(o, example, k) {
-        if (!typesMatch(o[k], example[k])) {
-            return false;
+    /**
+     * Usage: `verify(actual).vs(example);`.
+     * Options can contain the following properties:
+     *  - `evaluateGetters`: Verify getter contents.
+     *  - `treatFunctionsAsObjects`: Verify function own-properties.
+     */
+    looksLike.verify = function(source, o, options) {
+        if (!source || (typeof source !== 'string')) {
+            throw new Error('source not supplied. Client code should bind the source filename to the first argument.');
+        }
+        if (!isObject(o, options)) {
+            throw new Error('o must be an object.');
         }
 
-        if (isObject(example[k])) {
-            return areAlike(o[k], example[k]);
+        return {
+            vs: function(example) {
+                var result = looksLike(o, example, options);
+
+                if (!result.value) {
+                    throw new Error('invalid argument (verify the presence and type of the following property: ' + result.key + '). Source file: ' + source + '.');
+                }
+            }
+        };
+    };
+
+    /**
+     * Defaults to *not* evaluating getters.
+     */
+    function isAlike(o, example, options, returnValue, k) {
+        returnValue.key = k;
+
+        if (isGetter(example, k) && !shouldEvaluateGetters(options)) {
+            return spread(compareGetters, arguments);
+        }
+
+        if (isSetter(example, k)) {
+            return isSetter(o, k); // Based on the above, is *only* a setter.
+        }
+
+        if (!typesMatch(o[k], example[k])) {
+            return false; // Confirm basic type correspondence.
+        }
+
+        // Confirm contents of array (if example item is supplied).
+        if (Array.isArray(example[k]) && ('0' in example[k])) {
+            debugger;
+            return o[k].every(function(arrItem, index) {
+                // If value types, compare directly.
+                if (!isObject(example[k][0], options)) {
+                    return typesMatch(example[k][0], arrItem);
+                }
+
+                // Use normal comparison for objects.
+                return areAlike(arrItem, example[k][0], options, returnValue);
+            });
+        }
+
+        // If property is an object, confirm the contents, recursively.
+        if (isObject(example[k], options)) {
+            return areAlike(o[k], example[k], options, returnValue);
         }
 
         return true;
     }
 
-    function isObject(o) {
-        return o !== null && typeof o === 'object';
+    function isGetter(o, k) {
+        var descriptor = Object.getOwnPropertyDescriptor(o, k);
+
+        if (!descriptor) {
+            return false;
+        }
+
+        return !!(descriptor.get);
     }
 
+    function isSetter(o, k) {
+        var descriptor = Object.getOwnPropertyDescriptor(o, k);
+
+        if (!descriptor) {
+            return false;
+        }
+
+        return !!(descriptor.set);
+    }
+
+    function isObject(o, options) {
+        if (options && options.treatFunctionAsObject) {
+            return o !== null && (typeof o === 'object' || typeof o === 'function');
+        }
+
+        return o !== null && (typeof o === 'object');
+    }
+
+    /**
+     * Note 1: because typeof null === 'object'.
+     */
     function typesMatch(a, b) {
+        if (a === null) { // See note 1.
+            return a === b;
+        }
+
+        if (Array.isArray(b)) { // We use the example object here.
+            return Array.isArray(a);
+        }
+
         return typeof a === typeof b;
+    }
+
+    function areAlike(o, example, options, returnValue) {
+        return Object.keys(example)
+            .every(isAlike.bind(null, o, example, options, returnValue));
+    }
+
+    /**
+     * If options is null or undefined, 
+     * then returns false.
+     */
+    function shouldEvaluateGetters(options) {
+        return (!!options || (options && !options.evaluateGetters));
+    }
+
+    function compareGetters(o, example, options, returnValue, k) {
+        if (!isGetter(o, k)) {
+            return false; // Actual is not a matching getter.
+        }
+
+        if (isSetter(example, k)) {
+            return isSetter(o, k); // Example is also a setter, do they match?
+        }
+
+        return true; // Actual is a getter, like example.
+    }
+
+    function spread(fn, arr) {
+        fn.apply(null, arr);
     }
 
     namespace.looksLike = looksLike;
 
 }(namespace));
+
 
 	if ((typeof exports === 'object') && module) {
 		module.exports = namespace.looksLike; // CommonJS
--- spec/looks-like-spec.js
+++ spec/looks-like-spec.js
@@ -1,19 +1,26 @@
 /*global self, describe, it, expect*/
 'use strict';
 
-var looksLike = self.looksLike;
+var verify = self.looksLike.verify.bind(null, 'looks-like-spec');
 
-describe('looksLike', function() {
+describe('verify', function() {
 
-    [null, undefined, '', 0, true].forEach(function(testCase) {
-
+    [
+        null,
+        undefined,
+        '',
+        0,
+        true
+    ].forEach(function(testCase) {
 
         it('should throw an exception if o is not an object (' + testCase + ')', function() {
             //arrange
             var example = {};
 
             //act & assert
-            expect(looksLike.bind(null, testCase, example)).toThrow('o must be an object.');
+            expect(function() {
+                verify(testCase).vs(example);
+            }).toThrowError(Error, 'o must be an object.');
         });
 
         it('should throw an exception if example is not an object (' + testCase + ')', function() {
@@ -21,119 +28,250 @@ describe('looksLike', function() {
             var o = {};
 
             //act & assert
-            expect(looksLike.bind(null, o, testCase)).toThrow('example must be an object.');
+            expect(function() {
+                verify(o).vs(testCase);
+            }).toThrow('example must be an object.');
         });
 
     });
 
 
-    [null, undefined, 1, {}, true, '', 'foo', '1'].forEach(function(testCase) {
+    [
+        null,
+        undefined,
+        1, {},
+        true,
+        '',
+        'foo',
+        '1'
+    ].forEach(function(testCase) {
 
-        it('should return true if the object is like the example (' + testCase + ')', function() {
+        it('should not throw an exception if the object is like the example (' + testCase + ')', function() {
             //arrange
             var example = {
-            	foo: testCase
+                foo: testCase
             };
 
             //act & assert
-            expect(looksLike({
-            	foo: testCase
-            }, example)).toBe(true);
+            expect(verify({
+                foo: testCase,
+            }).vs(example)).toBe(undefined);
         });
 
     });
 
-    [[0,0],[0,1],[1,-1],[1,NaN]].forEach(function(testCase) {
+    [
+        [0, 0],
+        [0, 1],
+        [1, -1],
+        [1, NaN]
+    ].forEach(function(testCase) {
 
-        it('should return true if the object is like the example (number: ' + testCase + ')', function() {
+        it('should not throw an exception if the object is like the example (number: ' + testCase + ')', function() {
             //arrange
             var example = {
-            	foo: testCase[0]
+                foo: testCase[0]
             };
 
             //act & assert
-            expect(looksLike({
-            	foo: testCase[1]
-            }, example)).toBe(true);
+            expect(verify({
+                foo: testCase[1]
+            }).vs(example)).toBe(undefined);
         });
 
     });
 
-    [[true, false]].forEach(function(testCase) {
+    [
+        [true, false]
+    ].forEach(function(testCase) {
 
-        it('should return true if the object is like the example (boolean: ' + testCase + ')', function() {
+        it('should not throw an exception if the object is like the example (boolean: ' + testCase + ')', function() {
             //arrange
             var example = {
-            	foo: testCase[0]
+                foo: testCase[0]
             };
 
             //act & assert
-            expect(looksLike({
-            	foo: testCase[1]
-            }, example)).toBe(true);
+            expect(verify({
+                foo: testCase[1]
+            }).vs(example)).toBe(undefined);
         });
 
     });
 
-    [[{}, {}],[/foo/g, /bar/g],[[], []]].forEach(function(testCase) {
+    [
+        [{}, {}],
+        [/foo/g, /bar/g],
+        [
+            [],
+            []
+        ]
+    ].forEach(function(testCase) {
 
-        it('should return true if the object is like the example (object: ' + testCase + ')', function() {
+        it('should not throw an exception if the object is like the example (object: ' + testCase + ')', function() {
             //arrange
             var example = {
-            	foo: testCase[0]
+                foo: testCase[0]
             };
 
             //act & assert
-            expect(looksLike({
-            	foo: testCase[1]
-            }, example)).toBe(true);
+            expect(verify({
+                foo: testCase[1]
+            }).vs(example)).toBe(undefined);
         });
 
     });
 
-    [{}, {bam:1}].forEach(function(testCase) {
+    [{}, {
+        bam: 1
+    }].forEach(function(testCase) {
 
-        it('should return true if the object differs recursively (' + testCase + ')', function() {
+        it('should throw an exception if the object differs recursively (' + JSON.stringify(testCase) + ')', function() {
             //arrange
             var example = {
-            	foo: {
-            		bar: 1
-            	}
+                foo: {
+                    bar: 1
+                }
             };
 
             //act & assert
-            expect(looksLike({
-            	foo: testCase
-            }, example)).toBe(false);
+            expect(function() {
+                verify({
+                    foo: testCase
+                }).vs(example);
+            }).toThrowError(Error, 'invalid argument (verify the presence and type of the following property: bar). Source file: looks-like-spec.');
         });
 
     });
 
     [{
-    	bar: {
-    		bam: 'bam1',
-    		bat: null
-    	},
-    	baz: 42
+        bar: {
+            bam: 'bam1',
+            bat: null
+        },
+        baz: 42
     }].forEach(function(testCase) {
 
         it('should return true if a nested object does not differ recursively (' + testCase + ')', function() {
             //arrange
             var example = {
-            	foo: {
-            		bar: {
-            			bam: 'bam2'
-            		},
-            		baz: 1
-            	}
+                foo: {
+                    bar: {
+                        bam: 'bam2'
+                    },
+                    baz: 1
+                }
             };
 
             //act & assert
-            expect(looksLike({
-            	foo: testCase
-            }, example)).toBe(true);
+            expect(
+                verify({
+                    foo: testCase
+                }).vs(example)
+            ).toBe(undefined);
         });
 
+    });
+
+    it('should throw an exception if example defines a setter property and actual does not', function() {
+        //arrange
+        var example = {
+            foo: {
+                get bar() {
+                    return '';
+                },
+            }
+        };
+
+        //act & assert
+        expect(function() {
+            verify({
+                foo: {
+                    bar: 'bar',
+                }
+            }).vs(example);
+        }).toThrowError(Error, 'invalid argument (verify the presence and type of the following property: bar). Source file: looks-like-spec.');
+    });
+
+    it('should throw an exception if example defines a setter property and actual does not', function() {
+        //arrange
+        var example = {
+            foo: {
+                get bar() {},
+                set bar(v) {},
+            }
+        };
+
+        //act & assert
+        expect(function() {
+            verify({
+                foo: {
+                    get bar() {},
+                }
+            }).vs(example);
+        }).toThrowError(Error, 'invalid argument (verify the presence and type of the following property: bar). Source file: looks-like-spec.');
+    });
+
+    it('should not throw an exception if example defines a property and actual does not, but verify is configured to evaluate getters and they evaluate to the same type', function() {
+        //arrange
+        var example = {
+            foo: {
+                get bar() {
+                    return '';
+                },
+            }
+        };
+
+        //act & assert
+        expect(
+            verify({
+                foo: {
+                    bar: 'bar',
+                }
+            }, {
+                evaluateGetters: true
+            }).vs(example)).toBe(undefined);
+    });
+
+    it('should verify the contents of arrays per the example', function() {
+        //arrange
+        var example = {
+            foo: ['']
+        };
+
+        //act & assert
+        expect(function() {
+            verify({
+                foo: [0] // Second item!
+            }).vs(example);
+        }).toThrowError(Error, 'invalid argument (verify the presence and type of the following property: foo). Source file: looks-like-spec.');
+    });
+
+    it('should verify the contents of arrays per the example (multiple array items)', function() {
+        //arrange
+        var example = {
+            foo: ['']
+        };
+
+        //act & assert
+        expect(function() {
+            verify({
+                foo: ['', 0] // Second item!
+            }).vs(example);
+        }).toThrowError(Error, 'invalid argument (verify the presence and type of the following property: foo). Source file: looks-like-spec.');
+    });
+
+    it('should verify the contents of arrays per the example, and not throw if contents match', function() {
+        //arrange
+        var example = {
+            foo: ['']
+        };
+
+        //act & assert
+        expect(
+            verify({
+                foo: ['foo', 'bar']
+            }).vs(example)).toBe(undefined);
     });
 
 });
